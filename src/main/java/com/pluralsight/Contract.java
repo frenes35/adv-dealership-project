80c8dc53966cd75437e71697a45ed3ca438aeba0
Student <enesyilmaz4525@gmail.com>
2024-11-06 13:48:56 -0500

    SalesContract and LeaseContract classes have been done.

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,35 +1,32 @@
 package com.pluralsight;
 
-public class Contract {
+public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    protected double totalPrice;
+    protected double monthlyPayment;
 
-    public abstract class contract {
-        private String date;
-        private String customerName;
-        private String customerEmail;
-        private Vehicle vehicleSold;
-        protected double totalPrice;
-        protected double monthlyPayment;
-
-        public contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
-            this.date = date;
-            this.customerName = customerName;
-            this.customerEmail = customerEmail;
-            this.vehicleSold = vehicleSold;
-        }
-
-        public String getDate() { return date; }
-        public void setDate(String date) { this.date = date; }
-
-        public String getCustomerName() { return customerName; }
-        public void setCustomerName(String customerName) { this.customerName = customerName; }
-
-        public String getCustomerEmail() { return customerEmail; }
-        public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
-
-        public Vehicle getVehicleSold() { return vehicleSold; }
-        public void setVehicleSold(Vehicle vehicleSold) { this.vehicleSold = vehicleSold; }
-
-        public abstract double getTotalPrice();
-        public abstract double getMonthlyPayment();
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
     }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getCustomerName() { return customerName; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+
+    public String getCustomerEmail() { return customerEmail; }
+    public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
+
+    public Vehicle getVehicleSold() { return vehicleSold; }
+    public void setVehicleSold(Vehicle vehicleSold) { this.vehicleSold = vehicleSold; }
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
--- src/main/java/com/pluralsight/SalesContract.java
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -1,26 +1,36 @@
-@Override
-public double getTotalPrice() {
-        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
-        }
+package com.pluralsight;
 
-@Override
-public double getMonthlyPayment() {
-        int numberOfPayments = 0;
-        double interestRate = 0;
-        if (financeOption) {
-        if (getVehicleSold().getPrice() >= 10000) {
-        numberOfPayments = 48;
-        interestRate = 4.25 / 1200;
-        } else {
-        numberOfPayments = 24;
-        interestRate = 5.25 / 1200;
-        }
+public class SalesContract extends Contract {
+    private static final double SALES_TAX_RATE = 0.05;
+    private static final int RECORDING_FEE = 100;
+    private double processingFee;
+    private boolean financeOption;
 
-        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
-        monthlyPayment = Math.round(monthlyPayment * 100);
-        monthlyPayment /= 100;
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, boolean financeOption) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.financeOption = financeOption;
+        this.processingFee = vehicleSold.getPrice() < 10000 ? 295 : 495;
+        this.totalPrice = calculateTotalPrice(vehicleSold.getPrice());
+        this.monthlyPayment = financeOption ? calculateMonthlyPayment(vehicleSold.getPrice()) : 0;
+    }
+
+    private double calculateTotalPrice(double vehiclePrice) {
+        return vehiclePrice + (vehiclePrice * SALES_TAX_RATE) + RECORDING_FEE + processingFee;
+    }
+
+    private double calculateMonthlyPayment(double vehiclePrice) {
+        double rate = vehiclePrice >= 10000 ? 0.0425 : 0.0525;
+        int months = vehiclePrice >= 10000 ? 48 : 24;
+        return (vehiclePrice * (1 + rate * months / 12)) / months;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
         return monthlyPayment;
-        } else {
-        return 0.0;
-        }
-        }*/
+    }
+}
